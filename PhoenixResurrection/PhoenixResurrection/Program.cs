6405eac81783c7ba102ea819d429db6fc4c1d939
BerayS <bsezgin00@gmail.com>
2021-04-10 23:21:44 -0700

    First Commit

---
+++ PhoenixResurrection/PhoenixResurrection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace PhoenixResurrection {
+    class Program {
+        static void Main(string[] args) {
+            int input = int.Parse(Console.ReadLine());
+            double[,] array2Da = new double[input, 3];
+
+            int uBound0 = array2Da.GetUpperBound(0);
+            int uBound1 = array2Da.GetUpperBound(1);
+            for (int i = 0; i <= uBound0; i++) {
+                for (int j = 0; j <= uBound1; j++) {
+                    double asd = double.Parse(Console.ReadLine());
+                    array2Da[i, j] = asd;
+                }
+            }
+            int cols = array2Da.GetLength(0);
+            double result = 0;
+            for (int i = 0; i < cols; i++) {
+                result = Math.Pow(array2Da[i, 0],2) * (array2Da[i, 1] + 2 * array2Da[i, 2]);
+                Console.WriteLine(result);
+            }
+
+    }
+    }
+}
